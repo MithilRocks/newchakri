4911b5d1b5121dae99be48fad56ecd41a984e890
MithilRocks <mithilbhoras@yahoo.co.in>
2018-06-23 19:44:01 +0530

    heroku update

--- chakri/base.py
+++ chakri/base.py
@@ -21,11 +21,11 @@ def home():
     data = {}
     data['css'] = url_for('static', filename='css/bootstrap.min.css')
     data['js'] = url_for('static', filename='js/bootstrap.min.js')
-    
+
     data['bank_nifty_daily'] = bank_nifty_daily.angles()
     data['nifty_50_daily'] = nifty_50_daily.angles()
     
     return render_template('home.html', data = data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
