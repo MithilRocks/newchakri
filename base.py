18c167610bac6c20da8fc72dc13c4e5b0eeb893a
MithilRocks <mithilbhoras@yahoo.co.in>
2018-08-04 13:29:27 +0530

    daily weekly changes

--- base.py
+++ base.py
@@ -3,29 +3,59 @@ from levels import levels
 from datetime import date, timedelta
 import datetime
 import json
-from dateutil.relativedelta import relativedelta, FR
+from dateutil.relativedelta import relativedelta, MO, FR
 
 app = Flask(__name__)
 
-@app.route('/daily')
-def daily():
+@app.route('/nifty/daily')
+def nifty_daily():
+    data = daily("NIFTY 50")
+    return render_template('daily.html', data = data)
+
+@app.route('/bank-nifty/daily')
+def bnf_daily():
+    data = daily("BANKNIFTY")
+    return render_template('daily.html', data = data)
+
+@app.route('/nifty/weekly')
+def nifty_weekly():
+    data = weekly("NIFTY 50")
+    return render_template('daily.html', data = data)
+
+@app.route('/bank-nifty/weekly')
+def bnf_weekly():
+    data = weekly("BANKNIFTY")
+    return render_template('daily.html', data = data)
+
+def daily(symbol):
     end_date = datetime.date.today()
-    start_date = end_date - timedelta(days=7)
+    start_date = end_date - timedelta(days=10)
 
-    nifty_50_daily = levels.Daily("NIFTY 50", start_date, end_date)
-    nifty_50_daily.get_index_history()
-
-    bank_nifty_daily = levels.Daily("BANKNIFTY", start_date, end_date)
-    bank_nifty_daily.get_index_history() 
+    daily_details = levels.Daily(symbol, start_date, end_date)
+    daily_details.get_index_history() 
 
     data = {}
     data['css'] = url_for('static', filename='css/bootstrap.min.css')
     data['js'] = url_for('static', filename='js/bootstrap.min.js')
-
-    data['bank_nifty_daily'] = bank_nifty_daily.main()
-    data['nifty_50_daily'] = nifty_50_daily.main()
+    data['symbol'] = symbol
+    data['daily'] = daily_details.main()
     
-    return render_template('daily.html', data = data)
+    return data
+
+def weekly(symbol):
+    end_date = datetime.date.today() + relativedelta(weekday=FR(-1))
+    start_date = datetime.date.today() + relativedelta(weekday = MO(-7))
+
+    weekly_details = levels.Weekly(symbol, start_date, end_date)
+    weekly_details.get_index_history()
+
+    data = {}
+    data['css'] = url_for('static', filename='css/bootstrap.min.css')
+    data['js'] = url_for('static', filename='js/bootstrap.min.js')
+    data['symbol'] = symbol
+    data['daily'] = weekly_details.main()
+
+    return data
 
 if __name__ == "__main__":
     app.run()
--- levels/levels.py
+++ levels/levels.py
@@ -33,7 +33,6 @@ class Daily(Levels):
         """
         The main function where the magic happens  
         """
-        self.get_index_history()
         highs = self.data['High'].to_dict()
         lows = self.data['Low'].to_dict()
         closes = self.data['Close'].to_dict()
@@ -59,6 +58,11 @@ class Daily(Levels):
             
             if len(average_highs) == 3:
                 daily['levels'][date] = {}
+
+                daily['levels'][date]['high'] = highs[date]
+                daily['levels'][date]['low'] = lows[date]
+                daily['levels'][date]['close'] = closes[date]
+
                 daily['levels'][date]['h2'] = self.averages(list(average_highs)[1:])
                 daily['levels'][date]['h3'] = self.averages(average_highs)
 
@@ -68,23 +72,28 @@ class Daily(Levels):
                 daily['levels'][date]['l2'] = self.averages(list(average_lows)[1:])
                 daily['levels'][date]['l3'] = self.averages(average_lows)
 
-            daily['camrilla'][date] = {}
+                daily['levels'][date]['g1'] = self.gann_angle(daily['levels'][date]['high'], daily['levels'][date]['close'])
+                daily['levels'][date]['g2'] = self.gann_angle(daily['levels'][date]['close'], daily['levels'][date]['low'])
+                daily['levels'][date]['g3'] = self.gann_angle(daily['levels'][date]['low'], daily['levels'][date]['close'])
+                daily['levels'][date]['g4'] = self.gann_angle(daily['levels'][date]['close'], daily['levels'][date]['high'])
 
-            cam = Camrilla(highs[date], lows[date], closes[date])
-            pi = Pivot(highs[date], lows[date], closes[date])
+                daily['camrilla'][date] = {}
 
-            daily['camrilla'][date]['r1'] = cam.r1()
-            daily['camrilla'][date]['r2'] = cam.r2()
-            daily['camrilla'][date]['r3'] = cam.r3()
-            daily['camrilla'][date]['r4'] = cam.r4()
-            daily['camrilla'][date]['r5'] = cam.r5()
-            daily['camrilla'][date]['s1'] = cam.s1()
-            daily['camrilla'][date]['s2'] = cam.s2()
-            daily['camrilla'][date]['s3'] = cam.s3()
-            daily['camrilla'][date]['s4'] = cam.s4()
-            daily['camrilla'][date]['s5'] = cam.s5()
+                cam = Camrilla(highs[date], lows[date], closes[date])
+                pi = Pivot(highs[date], lows[date], closes[date])
 
-            daily['pivot'][date] = pi.pivot_levels()
+                daily['camrilla'][date]['r1'] = cam.r1()
+                daily['camrilla'][date]['r2'] = cam.r2()
+                daily['camrilla'][date]['r3'] = cam.r3()
+                daily['camrilla'][date]['r4'] = cam.r4()
+                daily['camrilla'][date]['r5'] = cam.r5()
+                daily['camrilla'][date]['s1'] = cam.s1()
+                daily['camrilla'][date]['s2'] = cam.s2()
+                daily['camrilla'][date]['s3'] = cam.s3()
+                daily['camrilla'][date]['s4'] = cam.s4()
+                daily['camrilla'][date]['s5'] = cam.s5()
+
+                daily['pivot'][date] = pi.pivot_levels()
 
             counter += 1
 
@@ -93,10 +102,99 @@ class Daily(Levels):
 class Weekly(Levels):
 
     def __init__(self, symbol, start_date, end_date):
-        pass
+        super().__init__(symbol, start_date, end_date)
 
     def main(self):
-        pass
+        highs = self.data['High'].to_dict()
+        lows = self.data['Low'].to_dict()
+        closes = self.data['Close'].to_dict()
+        dates = list(highs)
+
+        counter, highest_high, lowest_low = 0, 0, 0
+        data = {}
+
+        for date in dates:
+
+            if counter == 0:
+                highest_high = highs[date]
+                lowest_low = lows[date]
+
+            if highs[date] > highest_high:
+                highest_high = highs[date]
+
+            if lowest_low > lows[date]:
+                lowest_low = lows[date]
+
+            counter += 1
+
+            if counter == 5:
+                data[date] = {}
+                data[date]['High'] = highest_high
+                data[date]['Low'] = lowest_low
+                data[date]['Close'] = closes[date]
+                counter = 0
+
+        average_highs = deque()
+        average_lows = deque()
+        average_closes = deque()
+
+        counter, weekly = 0, {}
+        weekly['levels'] = {}
+        weekly['camrilla'] = {}
+        weekly['pivot'] = {}
+
+        for date, levels in data.items():
+            average_highs.extend([levels['High']])
+            average_lows.extend([levels['Low']])
+            average_closes.extend([levels['Close']])
+
+            if counter >= 3:
+                average_highs.popleft()
+                average_lows.popleft()
+                average_closes.popleft()
+            
+            if len(average_highs) == 3:
+                weekly['levels'][date] = {}
+
+                weekly['levels'][date]['high'] = levels['High']
+                weekly['levels'][date]['low'] = levels['Low']
+                weekly['levels'][date]['close'] = levels['Close']
+
+                weekly['levels'][date]['h2'] = self.averages(list(average_highs)[1:])
+                weekly['levels'][date]['h3'] = self.averages(average_highs)
+
+                weekly['levels'][date]['c2'] = self.averages(list(average_closes)[1:])
+                weekly['levels'][date]['c3'] = self.averages(average_closes)
+
+                weekly['levels'][date]['l2'] = self.averages(list(average_lows)[1:])
+                weekly['levels'][date]['l3'] = self.averages(average_lows)
+
+                weekly['levels'][date]['g1'] = self.gann_angle(weekly['levels'][date]['high'], weekly['levels'][date]['close'])
+                weekly['levels'][date]['g2'] = self.gann_angle(weekly['levels'][date]['close'], weekly['levels'][date]['low'])
+                weekly['levels'][date]['g3'] = self.gann_angle(weekly['levels'][date]['low'], weekly['levels'][date]['close'])
+                weekly['levels'][date]['g4'] = self.gann_angle(weekly['levels'][date]['close'], weekly['levels'][date]['high'])
+
+                weekly['camrilla'][date] = {}
+
+                cam = Camrilla(levels['High'], levels['Low'], levels['Close'])
+                pi = Pivot(levels['High'], levels['Low'], levels['Close'])
+
+                weekly['camrilla'][date]['r1'] = cam.r1()
+                weekly['camrilla'][date]['r2'] = cam.r2()
+                weekly['camrilla'][date]['r3'] = cam.r3()
+                weekly['camrilla'][date]['r4'] = cam.r4()
+                weekly['camrilla'][date]['r5'] = cam.r5()
+                weekly['camrilla'][date]['s1'] = cam.s1()
+                weekly['camrilla'][date]['s2'] = cam.s2()
+                weekly['camrilla'][date]['s3'] = cam.s3()
+                weekly['camrilla'][date]['s4'] = cam.s4()
+                weekly['camrilla'][date]['s5'] = cam.s5()
+
+                weekly['pivot'][date] = pi.pivot_levels()
+
+            counter += 1
+
+        return weekly
 
 class Camrilla:
     
